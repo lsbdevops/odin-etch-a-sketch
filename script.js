6ce9558e44b9f776508be8a082bb138066f2c68f
lsbdevops <lsbdevops@gmail.com>
2023-09-02 23:32:00 +1000

    Add ability to change grid size
    Functionality added to change grid size button, and a number
    between 1-100 can be input to create a grid of 1-100 x 1-100.
    This provides the artist with more cells to use for their
    drawings - the total width/height of the grid is not changed
    only the 'resolution'.
    This has involved adding a function to delete the current grid.
    A new grid is then created and event listeners re-created for
    all cells.

--- script.js
+++ script.js
@@ -20,21 +20,48 @@ function createGrid(height, width) {
     }
 }
 
+function deleteGrid() {
+    // Delete row-wrapper.
+    document.querySelector(".row-wrapper").remove();
+
+    // Create a new (empty) row-wrapper div and append to page-wrapper.
+    const containerElement = document.createElement("div");
+    containerElement.classList.add("row-wrapper");
+
+    document.querySelector(".page-wrapper").appendChild(containerElement);
+}
+
 function changeCell(event)
 {
     // Add coloured class to cell, if the left mouse button is currently clicked.
     if ((event.type === "click") || (event.buttons === 1)) this.classList.add("coloured");
 }
 
-// Create a 16x16 grid of div elements ("cells").
-createGrid(16, 16);
+function addCellListeners() {
+    // Add mouseover and click event listener to all cells. Run function to change class of cell if triggered.
+    const cells = document.querySelectorAll(".cell");
 
-// Add mouseover and click event listener to all cells. Run function to change class of cell if triggered.
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", changeCell);
-    cell.addEventListener("click", changeCell);
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", changeCell);
+        cell.addEventListener("click", changeCell);
+    })
 }
-)
+
+// Create a 16x16 grid of div elements ("cells") and add event listeners.
+createGrid(16, 16);
+addCellListeners()
+
+const changeGridButton = document.querySelector("#change-grid");
+
+changeGridButton.addEventListener("click", (event) => {
+    // Prompt user for new grid size- accept only a number between 1-100.
+    let gridSize = +(prompt("Enter new grid size (1 - 100):"));
+    if((!gridSize) || (gridSize < 1) || (gridSize > 100)) return;
+
+    // Delete the current grid, create a new grid and add event listeners.
+    deleteGrid();
+    createGrid(gridSize, gridSize);
+    addCellListeners();
+} )
+
 
