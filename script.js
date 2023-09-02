0932d4ee9a87fbd2f3cb7f6bd4ede7b59730b2a5
lsbdevops <lsbdevops@gmail.com>
2023-09-02 21:44:51 +1000

    Add colouring of cells when clicked/hovered over
    Class of 'coloured' added to cells when clicked/hovered over
    allowing users to 'draw' on the grid.
    Additionally, the creation of the grid has been moved into
    its own function and updated to allow for a variable input
    of height/width.

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// Create a 16x16 grid of div elements.
+function createGrid(height, width) {
+    // Create a height x width grid of div elements.
+    const containerElement = document.querySelector(".row-wrapper")
 
-const containerElement = document.querySelector(".row-wrapper")
-
-// For every row.
-for(let i = 0; i < 16; i++) 
-{
-    // Create a row container.
-    const rowElement = document.createElement("div");
-    rowElement.classList.toggle("row");
-    containerElement.appendChild(rowElement);
-
-    // Create 16 div elements per row.
-    for(let i = 0; i < 16; i++)
+    // For every row.
+    for(let i = 0; i < height; i++) 
     {
-        const divElement = document.createElement("div");
-        divElement.classList.toggle("cell");
-        rowElement.appendChild(divElement);
+        // Create a row container.
+        const rowElement = document.createElement("div");
+        rowElement.classList.toggle("row");
+        containerElement.appendChild(rowElement);
+
+        // Create 16 div elements per row.
+        for(let i = 0; i < width; i++)
+        {
+            const divElement = document.createElement("div");
+            divElement.classList.toggle("cell");
+            rowElement.appendChild(divElement);
+        }
     }
 }
+
+function changeCell(event)
+{
+    // Add coloured class to cell, if the left mouse button is currently clicked.
+    if ((event.type === "click") || (event.buttons === 1)) this.classList.add("coloured");
+}
+
+// Create a 16x16 grid of div elements ("cells").
+createGrid(16, 16);
+
+// Add mouseover and click event listener to all cells. Run function to change class of cell if triggered.
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", changeCell);
+    cell.addEventListener("click", changeCell);
+}
+)
+
